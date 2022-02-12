152d5332211fabcea1d2f321b5b053b6dc2fac7b
KachmarDmitriu <Kachmar.Dmitriu@ymail.com>
2022-02-12 02:14:33 +0200

    исправлены некоторые ошибки, но БД не создается (не нашел на эмуляторе)

---
+++ app/src/main/java/com/example/webradioplayer/dao/database/URL_for_MediaItem.kt
@@ -0,0 +1,8 @@
+package com.example.webradioplayer.dao.database
+
+import androidx.room.ColumnInfo
+
+class URL_for_MediaItem {
+    @ColumnInfo(name = "url_radiostation")
+    var urlRadiostation: String?  = null
+}
