8cd3b4ff9370793b33d1edbcac97c8124939e0f3
kachmar.dmitiu@ymail.com <93187112dm>
2022-05-27 18:52:52 +0300

    создание репозитория и рециклера промежуточное сохранение_ part.4

--- app/src/main/java/com/example/webradioplayer/ui/CustomAdapter.kt
+++ app/src/main/java/com/example/webradioplayer/ui/CustomAdapter.kt
@@ -1,39 +1,34 @@
    package com.example.webradioplayer.ui
 
-import android.app.TaskStackBuilder.create
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ListAdapter
 import android.widget.TextView
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
-import androidx.sqlite.db.SimpleSQLiteQuery.bind
 import com.example.webradioplayer.R
 import com.example.webradioplayer.dao.database.ListRadiostation
-import com.example.webradioplayer.dao.database.WebPlayerDatabase
-import kotlinx.coroutines.NonDisposableHandle.parent
 
 
-
-
-   class CustomAdaper(val  items: List<String>) : RecyclerView.Adapter<CustomAdaper.ViewHolder>(WordsComparator()) {
+   class CustomAdaper(val  items: List<String>) : RecyclerView.Adapter<CustomAdaper.ViewHolder>()//LISTRADIO_COMPARATOR)
+   {
 //class CustomAdaper(val  items: List<String>):ListAdapter<ListRadiostation, CustomAdaper.ViewHolder>(ListRadiostationComparator()) {
 
 
        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
 
-           val view = LayoutInflater.from(parent.context)
-               .inflate(R.layout.list_note_radiostation, parent, false)
+          // val view = LayoutInflater.from(parent.context)
+            //   .inflate(R.layout.list_note_radiostation, parent, false)
 
-           return ViewHolder(view)
+          // return ViewHolder(view)
 
+            return ViewHolder.create(parent)
        }
 
        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
-           val text = items[position]
-           holder.textView.text = text
+           val current = (position)
+           holder.bind(current)  //textView.text = text
 
        }
 
@@ -42,6 +37,32 @@ import kotlinx.coroutines.NonDisposableHandle.parent
        }
 
 
+       class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
+       {
+         //  val textView: TextView
+
+         //  init {
+         //      textView = itemView as TextView
+         //  }
+
+           private val listRadiostationItemView: TextView = itemView.findViewById(R.id.name)//textView)
+
+       fun bind(text: Int) {
+           listRadiostationItemView.text = text.toString()
+       }
+
+       companion object {
+           fun create(parent: ViewGroup): ViewHolder
+           {
+               val view: View = LayoutInflater.from(parent.context)
+                   .inflate(R.layout.list_note_radiostation, parent, false)
+               return ViewHolder(view)
+           }
+       }
+   }
+
+
+/*
        class ListRadiostationComparator  : DiffUtil.ItemCallback<ListRadiostation>() {
            override fun areItemsTheSame(
                oldItem: ListRadiostation,
@@ -62,36 +83,6 @@ import kotlinx.coroutines.NonDisposableHandle.parent
            }
        }
 
-
-
-       inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)
-        {
-            val textView: TextView
-
-            init {
-                textView = itemView as TextView
-            }
-
-
-
-           private val wordItemView: TextView = itemView.findViewById(R.id.textView)
-
-           fun bind(text: String?) {
-               wordItemView.text = text
-           }
-
-           companion object {
-               fun create(parent: ViewGroup): ViewHolder
-                {
-                   val view: View = LayoutInflater.from(parent.context)
-                       .inflate(R.layout.list_note_radiostation, parent, false)
-                   return ViewHolder(view)
-               }
-           }
-       }
-
-
-
    class WordsComparator : DiffUtil.ItemCallback<ListRadiostation>() {
        override fun areItemsTheSame(oldItem: ListRadiostation, newItem: ListRadiostation): Boolean {
            return oldItem === newItem
@@ -104,10 +95,10 @@ import kotlinx.coroutines.NonDisposableHandle.parent
                    oldItem.urlRadiostation == newItem.urlRadiostation
        }
 
-
+*/
 
    companion object {
-           private val WORDS_COMPARATOR = object : DiffUtil.ItemCallback<ListRadiostation>() {
+           private val LISTRADIO_COMPARATOR = object : DiffUtil.ItemCallback<ListRadiostation>() {
                override fun areItemsTheSame(oldItem: ListRadiostation, newItem: ListRadiostation): Boolean {
                    return oldItem === newItem
                }
@@ -123,7 +114,7 @@ import kotlinx.coroutines.NonDisposableHandle.parent
 
    }
 
-   }
+  // }
 
 
 
