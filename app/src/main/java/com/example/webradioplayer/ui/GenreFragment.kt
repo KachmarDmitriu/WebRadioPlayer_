e873ca8188f348fd329205dd67e81d7d586e9747
kachmar.dmitiu@ymail.com <93187112dm>
2022-10-09 19:19:09 +0300

    промежуточное сохранение 2
    работа с
    genre_fragment.xml
    GenreFragment.kt
    GenreAdapter.kt
    
    **********
    Сделано
    
    добавлено в PlaylistViewModel.kt и GenreViewModel.kt   - функции insert
    
    добавлено DataRepository.kt - функции addRadiostation, addGenre
    
    содано разметка player_activity.xml, select_genre_fragment.xml, playlist_fragment.xml
    
    ***********
    Для справки
    
    GenreViewModel.kt = ProductListviewModel
    
    PlaylistViewModel = ProductViewModel
    
    за основу в обеих последних, вероятно, ProductViewModel
    
    ***************
    
    Нужно реализовать
    *- Genre/Playlist ClickCallback - готово (используется в Genre/Playlist фрагментах в разметке);
    
    *- Genre/Playlist Fragments - раземетка (+ Genre/Playlist Item), если возможно- использовать дизайн из существующего;
    
    *- Genre/Playlist fragment - бизнес логика;
    
    *- Genre/Playlist Adapter -;
    
    *-  PlayerActivity - добавить вызов фрагмента Playlist? (Genre)?

--- app/src/main/java/com/example/webradioplayer/ui/GenreFragment.kt
+++ app/src/main/java/com/example/webradioplayer/ui/GenreFragment.kt
@@ -10,16 +10,22 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModelProvider
+import androidx.fragment.app.viewModels
 import com.example.webradioplayer.R
 import com.example.webradioplayer.adapters.GenreAdapter
+import com.example.webradioplayer.database.entity.Genre
 import com.example.webradioplayer.databinding.GenreFragmentBinding
 import com.example.webradioplayer.ui.Callback.IGenreClickCallback
+import com.example.webradioplayer.viewmodel.GenreViewModel
+
 
 class GenreFragment: Fragment() {
 
   //  class ProductListFragment : Fragment() {
         private var mGenreAdapter: GenreAdapter? = null
         private var mBinding: GenreFragmentBinding? = null
+
+    private val viewModel by viewModels<GenreViewModel>()
 
     private val binding get() = requireNotNull(mBinding)  // ?? что делает?? из проекта Артура
 
@@ -37,6 +43,41 @@ class GenreFragment: Fragment() {
 
         override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
             super.onViewCreated(view, savedInstanceState)
+            init()
+        }
+
+
+    private fun init() {
+        setupRefresh()
+        setupRecycler()
+        observeUiState()
+
+        viewModel.loadCountries()
+    }
+
+    private fun setupRefresh() {
+        binding.swipeRefresh.setOnRefreshListener {
+            viewModel.loadCountries(true)
+        }
+    }
+
+    private fun setupRecycler() {
+        binding.recyclerCountries.adapter = countriesAdapter
+    }
+
+    private fun observeUiState() {
+        viewModel.state.observe(viewLifecycleOwner, ::handleUiState)
+    }
+
+    private fun handleUiState(state: CountriesUiState) {
+        when (state) {
+            is CountriesLoadError -> onError(state.message)
+            is CountriesLoaded -> onCountriesLoaded(state.data)
+            is CountriesLoading -> binding.swipeRefresh.isRefreshing = state.isLoading
+        }
+    }
+
+            /*  для поиска в списке, пока не используется,
             val viewModel: ProductListViewModel =
                 ViewModelProvider(this).get(ProductListViewModel::class.java)
             mBinding.productsSearchBtn.setOnClickListener { v ->
@@ -44,16 +85,16 @@ class GenreFragment: Fragment() {
                 viewModel.setQuery(query)
             }
             subscribeUi(viewModel.getProducts())
-        }
+      }
 
-        private fun subscribeUi(liveData: LiveData<List<ProductEntity?>>) {
+        private fun subscribeUi(liveData: LiveData<List<Genre>>) {
             // Update the list when the data changes
             liveData.observe(
                 viewLifecycleOwner
-            ) { myProducts: List<ProductEntity?>? ->
-                if (myProducts != null) {
+            ) { myGenre: List<Genre>? ->
+                if (myGenre != null) {
                     mBinding.setIsLoading(false)
-                    mGenreAdapter.setProductList(myProducts)
+                    mGenreAdapter.setProductList(myGenre)
                 } else {
                     mBinding.setIsLoading(true)
                 }
@@ -62,6 +103,8 @@ class GenreFragment: Fragment() {
                 mBinding.executePendingBindings()
             }
         }
+    */
+
 
         override fun onDestroyView() {
             mBinding = null
