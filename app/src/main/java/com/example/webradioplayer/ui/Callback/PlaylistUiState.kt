694e62984478510621fb28042b8d4e299791d558
kachmar.dmitiu@ymail.com <93187112dm>
2022-11-05 19:29:53 +0200

    промежуточное сохранение 1.0.5
    продолжить
    GenreFragment.kt
    
    возможно есть способ передать выбранный жанр лучше
    не очень понятно как работают
     GenresUiState.kt
     PlaylistUiState.kt
    
     последний блок в PlaylistViewModel.kt
    
     onGenreClick в GenreFragment

---
+++ app/src/main/java/com/example/webradioplayer/ui/Callback/PlaylistUiState.kt
@@ -0,0 +1,7 @@
+package com.example.webradioplayer.ui
+
+import com.example.webradioplayer.model.IGenre
+
+//sealed class PlaylistUiState
+sealed class PlaylistUiState
+data class GenreSelected(val genre: IGenre) : GenresUiState()
--- app/src/main/java/com/example/webradioplayer/viewmodel/PlaylistViewModel.kt
+++ app/src/main/java/com/example/webradioplayer/viewmodel/PlaylistViewModel.kt
@@ -3,6 +3,9 @@ package com.example.webradioplayer.viewmodel
 import androidx.lifecycle.*
 import com.example.webradioplayer.DataRepository
 import com.example.webradioplayer.database.entity.Playlist
+import com.example.webradioplayer.model.IGenre
+import com.example.webradioplayer.ui.GenreSelected
+import com.example.webradioplayer.ui.GenresUiState
 import kotlinx.coroutines.launch
 
 class PlaylistViewModel(private val repository: DataRepository): ViewModel() {
@@ -24,6 +27,17 @@ class PlaylistViewModel(private val repository: DataRepository): ViewModel() {
     fun insert(playlist: Playlist) = viewModelScope.launch {
         repository.addRadiostation(playlist)
     }
+
+
+
+    private val _state: MutableLiveData<GenresUiState> = MutableLiveData()
+    val state: LiveData<GenresUiState> = _state
+
+    fun selectCountry(genre: IGenre) {
+        _state.value =  GenreSelected(genre)
+    }
+
+
 
 }
 
